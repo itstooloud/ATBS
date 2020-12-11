bf20fedb607afecabdde24905c8336823add1ff0
Chris Collingwood <itstooloud@gmail.com>
2020-12-10 22:05:15 -0500

    third commit

---
+++ CH6_Strings/catNapping.py
@@ -0,0 +1,8 @@
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+#note that the single character in Eve's does not need to be escaped because of the triple quotes
